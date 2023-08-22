692dd6f62f7536237394cbf6752fe52b97008496
Syed Aslam <syedaslam.me@gmail.com>
2023-08-22 20:52:17 +0530

    implement add to cart operation

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("64e4c3a228475a03a48b0429")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
